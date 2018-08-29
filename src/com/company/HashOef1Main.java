6c08193da4380a13ea4fa5931d8cf46fe3ef5d03
Popeye985 <ceulemans_koen@hotmail.com>
2018-08-29 13:14:45 +0200

    Initial commit

---
+++ src/com/company/HashOef1Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Objects;
+
+public class HashOef1Main {
+    public static void main(String[] args) {
+        HashSet<Auto> databank = new HashSet<>();
+        databank.add(new Auto("1-SKY", "Karen"));
+        databank.add(new Auto("1-ABC-123", "Kristel"));
+        databank.add(new Auto("19-GR-LL", "Josje"));
+        databank.add(new Auto("1-SKY", "Marthe"));
+        for (Auto auto : databank) {
+            System.out.printf("%s bestuurd door %s%n", auto.getNummerplaat(), auto.getBestuurder());
+        }
+    }
+}
+
+class Auto {
+    private String nummerplaat;
+    private String bestuurder;
+
+    public Auto(String nummerplaat, String bestuurder) {
+        this.nummerplaat = nummerplaat;
+        this.bestuurder = bestuurder;
+    }
+
+
+    public String getBestuurder() {
+        return this.bestuurder;
+    }
+
+    public String getNummerplaat() {
+        return this.nummerplaat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Auto auto = (Auto) o;
+        return Objects.equals(nummerplaat, auto.nummerplaat);
+
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(nummerplaat);
+    }
+
+
+}
